4c9aefe4ef68f2189729edac2186cf5a94f3efe9
liwuyang <liwuyang@lbesec.com>
2018-03-30 17:02:00 +0800

    Initial Commit

---
+++ runtimeTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  runtimeTest
+//
+//  Created by lbe on 2018/3/30.
+//  Copyright © 2018年 liwuyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
